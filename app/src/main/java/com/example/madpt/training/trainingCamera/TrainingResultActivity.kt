8c6cecce9004272d33e4d8bf1271d812e86aefa6
kr <jij7401@naver.com>
2022-04-28 19:51:25 +0900

    FEAT: 운동 종료 화면 및 운동 데이터 통신용 class 제작

---
+++ app/src/main/java/com/example/madpt/training/trainingCamera/TrainingResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madpt.training.trainingCamera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrainingResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training_result)
+    }
+}
---
+++ app/src/main/java/com/example/madpt/training/trainingCamera/data/TrainingData.kt
@@ -0,0 +1,4 @@
+package com.example.madpt.training.trainingCamera.data
+
+class trainingData {
+}

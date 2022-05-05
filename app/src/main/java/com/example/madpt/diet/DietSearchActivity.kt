55af25b492c595c747c8634441c7a378958af2c4
sungjun-an <hiahn777@naver.com>
2022-05-05 10:33:41 +0900

    [FEAT]: [#10] [음식 가져오기 API 완성]
    서버와 통신하여 음식 리스트 가져오는 API 완성, 서버 통신 중 로딩 화면 구
    현

--- app/src/main/java/com/example/madpt/diet/DietSearchActivity.kt
+++ app/src/main/java/com/example/madpt/diet/DietSearchActivity.kt
@@ -1,38 +1,29 @@
 package com.example.madpt.diet
 
 import android.content.Intent
-import android.graphics.PointF.length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
-import android.widget.AdapterView
-import androidx.core.util.ObjectsCompat.toString
-import com.example.madpt.*
 import com.example.madpt.API.food.GetFoodList
 import com.example.madpt.API.food.GetFoodListCall
 import com.example.madpt.API.food.food_list
 import com.example.madpt.databinding.ActivityDietSearchBinding
 
-
 class DietSearchActivity : AppCompatActivity(), GetFoodList {
+
+
     private lateinit var binding: ActivityDietSearchBinding
 
     private var food_search_result_list = ArrayList<food_list>()
-
-    private var viewList = ArrayList<food_list>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDietSearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewList.addAll(food_search_result_list)
         setupEvents()
 
         binding.foodSearchListview.setOnItemClickListener{parent,view,position,id ->
-            val clickedRoom = food_search_result_list[position]
+            val clickedRoom: food_list = food_search_result_list[position]
             val mIntent = Intent(this,SearchFoodDataModifySaveActivity::class.java)
             mIntent.putExtra("roomInfo",clickedRoom)
             startActivity(mIntent)
@@ -46,9 +37,7 @@ class DietSearchActivity : AppCompatActivity(), GetFoodList {
             var textlengh: Int = binding.foodSearchBar.text.length
             food_search_result_list.clear()
             var foodSearchText = binding.foodSearchBar.text.toString()
-
-            GetFoodListCall(this).Get(foodSearchText)
-
+            GetFoodListCall(this, this).Get(foodSearchText)
 //            for (i in FoodList.indices) {
 //                var temp_food_maker: String?
 //                if (FoodList[i].maker_name.toString()

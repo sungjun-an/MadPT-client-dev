b1df479e6d91b370764ce7622366e525ceb785b6
SungHwanKing <dksj9921@gmail.com>
2022-05-12 02:38:11 +0900

    [FEAT] : SocialPage 완료

--- app/src/main/java/com/example/madpt/social/FriendsListAdapter.kt
+++ app/src/main/java/com/example/madpt/social/FriendsListAdapter.kt
@@ -7,29 +7,57 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madpt.FriendsData
+import com.example.madpt.FriendsDataList
 import com.example.madpt.R
+import com.example.madpt.databinding.FriedsListBinding
+import com.example.madpt.inputTestFriendsData
 import com.example.madpt.training.LoadRecyclerAdapter
 import com.example.madpt.training.TrainingAdapter
 import kotlinx.coroutines.NonDisposableHandle.parent
 
-class FriendsListAdapter(private val context: Context) : RecyclerView.Adapter<FriendsListAdapter.ViewHolder>() {
+class FriendsListAdapter(private val context: Context) :
+    RecyclerView.Adapter<FriendsListAdapter.FriendViewHolder>() {
 
 
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+    class FriendViewHolder(val binding: FriedsListBinding) : RecyclerView.ViewHolder(binding.root)
 
-    }
-    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
-        val v = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.frieds_list, viewGroup, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): FriendViewHolder {
+
+        val v =
+            LayoutInflater.from(viewGroup.context).inflate(R.layout.frieds_list, viewGroup, false)
+        return FriendViewHolder(FriedsListBinding.bind(v))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return FriendsDataList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(viewHolder: FriendViewHolder, position: Int) {
+        val show = FriendsDataList[position]
 
+        when (position) {
+            0 -> {
+                viewHolder.binding.medal.setImageResource(R.drawable.gold_medal)
+                viewHolder.binding.rank.visibility = View.INVISIBLE
+            }
+            1 -> {
+                viewHolder.binding.medal.setImageResource(R.drawable.silver_medal)
+                viewHolder.binding.rank.visibility = View.INVISIBLE
+            }
+            2 -> {
+                viewHolder.binding.medal.setImageResource(R.drawable.bronze_medal)
+                viewHolder.binding.rank.visibility = View.INVISIBLE
+            }
+            else -> {
+                viewHolder.binding.medal.visibility = View.INVISIBLE
+                viewHolder.binding.rank.visibility = View.VISIBLE
+            }
+
+        }
+        viewHolder.binding.rank.setText((position+1).toString())
+        viewHolder.binding.friendName.text = show.name
+        viewHolder.binding.kcalScore.text = show.useKcal.toString()
+
+    }
 }
+
